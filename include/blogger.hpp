96755f299a3444995f154d4d18f4c06b1226352a
Phillip Hamnett <phillip.hamnett@chintai.io>
2022-10-11 10:33:44 +0200

    Add skeleton functions and actions that are required

--- include/blogger.hpp
+++ include/blogger.hpp
@@ -1,4 +1,5 @@
 #include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
 
 #include <string>
 
@@ -7,4 +8,7 @@ class [[eosio::contract("blogger")]] blogger : public eosio::contract {
       using eosio::contract::contract;
 
       [[eosio::action]] void addpost(eosio::name user, std::string post);
+      [[eosio::action]] void deletepost(uint64_t id, eosio::name user); // User here is the post creator, needed for require_auth
+      [[eosio::action]] void ratepost(uint64_t id, eosio::name user, int rating); // User here is the person rating the post, needed for require_auth and table
+      [[eosio::on_notify("*::transfer")]] void on_transfer(eosio::name from, eosio::name to, eosio::asset quantity, std::string memo);
 };
--- src/blogger.cpp
+++ src/blogger.cpp
@@ -2,3 +2,9 @@
 
 void blogger::addpost(eosio::name user, std::string post){
 }
+void blogger::deletepost(uint64_t id, eosio::name user){
+}
+void blogger::ratepost(uint64_t id, eosio::name user, int rating){
+}
+void blogger::on_transfer(eosio::name from, eosio::name to, eosio::asset quantity, std::string memo){
+}
