06b7fec343429b380a2460f6fc668cd36f94da88
jennibearduit <77873928+jennibearduit@users.noreply.github.com>
2022-10-12 21:30:23 +0800

    make attributes private and add getters

--- include/post.hpp
+++ include/post.hpp
@@ -4,12 +4,14 @@
 #include <string>
 
 class [[eosio::table, eosio::contract("blogger")]] post {
-  public:
+  private:
     uint64_t post_id;
     eosio::name user;
     std::string content;
 
     uint64_t primary_key() const {return post_id;}
+    eosio::name get_user() const {return user;}
+    std::string get_contet() const {return content;}
 
     EOSLIB_SERIALIZE(post, (post_id)(user)(content));
 };
