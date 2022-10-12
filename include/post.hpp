4e11a832cbcde9fd15ebcc8343469495f737df7b
jennibearduit <77873928+jennibearduit@users.noreply.github.com>
2022-10-12 21:51:26 +0800

    update post

--- include/post.hpp
+++ include/post.hpp
@@ -4,12 +4,31 @@
 #include <string>
 
 class [[eosio::table, eosio::contract("blogger")]] post {
-  public:
+  private:
+    // attributes
     uint64_t post_id;
     eosio::name user;
     std::string content;
 
+    // getters
     uint64_t primary_key() const {return post_id;}
+    eosio::name get_user() const {return user;}
+    std::string get_content() const {return content;}
+
+    // setters
+    void set_post_id(uint64_t x) {post_id = x;}
+    void set_user(eosio::name x) {post_id = x;}
+    void set_content(std::string x) {content = x;}
+  
+  public:
+    // constructor 1
+    post(){} 
+    // constructor 2
+    post(uint64_t post_id, eosio::name user, std::string content) { 
+      post_id = _post_id;
+      user = _user;
+      content = _content;
+    }
 
     EOSLIB_SERIALIZE(post, (post_id)(user)(content));
 };
