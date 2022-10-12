49ee0be1801243207e9a33acf17f02aaf5ee7787
jennibearduit <77873928+jennibearduit@users.noreply.github.com>
2022-10-13 00:10:42 +0800

    bug fix

--- src/blogger.cpp
+++ src/blogger.cpp
@@ -2,15 +2,15 @@
 
 // dfthomework3
 void blogger::addpost(eosio::name user, std::string content){
-    deal_table table(_self, _self.value);
-    deal tmp_post(table.available_primary_key(), user, content);
+    post_table table(_self, _self.value);
+    post tmp_post(table.available_primary_key(), user, content);
     table.emplace(_self, [&](auto & entry) { 
         entry = tmp_post; 
     })
 }
 void blogger::deletepost(uint64_t id, eosio::name user){
     require_auth(_self);
-    deal_table table(_self, _self.value);
+    post_table table(_self, _self.value);
     auto itr = table.find(id);
     eosio::check(itr != table.end(), "A post does not exist with this ID");
     eosio::check(has_auth(itr->get_user()) || has_auth(_self), "Missing authority to delete this post");
@@ -18,7 +18,7 @@ void blogger::deletepost(uint64_t id, eosio::name user){
 }
 void blogger::ratepost(uint64_t id, eosio::name user, int rating){
     require_auth(_self);
-    deal_table table(_self, _self.value);
+    post_table table(_self, _self.value);
 }
 void blogger::on_transfer(eosio::name from, eosio::name to, eosio::asset quantity, std::string memo){
 }
