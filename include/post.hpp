08b1535320736479476262266c1c3abb264a0a94
jennibearduit <77873928+jennibearduit@users.noreply.github.com>
2022-10-13 00:06:23 +0800

    edit post

--- include/post.hpp
+++ include/post.hpp
@@ -5,25 +5,20 @@
 
 class [[eosio::table, eosio::contract("blogger")]] post {
   private:
-    // attributes
     uint64_t post_id;
     eosio::name user;
     std::string content;
 
-    // getters
+ public:
     uint64_t primary_key() const {return post_id;}
     eosio::name get_user() const {return user;}
     std::string get_content() const {return content;}
 
-    // setters
     void set_post_id(uint64_t x) {post_id = x;}
     void set_user(eosio::name x) {user = x;}
     void set_content(std::string x) {content = x;}
-  
-  public:
-    // constructor 1
-    post(){} 
-    // constructor 2
+
+    post(){}
     post(uint64_t _post_id, eosio::name _user, std::string _content) {
       post_id = _post_id;
       user = _user;
