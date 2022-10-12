41f2bd5358734b42dbc260ed6a7bac4bf758a9e9
jennibearduit <77873928+jennibearduit@users.noreply.github.com>
2022-10-13 00:04:43 +0800

    implement deletepost and ratepost

--- include/post.hpp
+++ include/post.hpp
@@ -17,14 +17,14 @@ class [[eosio::table, eosio::contract("blogger")]] post {
 
     // setters
     void set_post_id(uint64_t x) {post_id = x;}
-    void set_user(eosio::name x) {post_id = x;}
+    void set_user(eosio::name x) {user = x;}
     void set_content(std::string x) {content = x;}
   
   public:
     // constructor 1
     post(){} 
     // constructor 2
-    post(uint64_t post_id, eosio::name user, std::string content) { 
+    post(uint64_t _post_id, eosio::name _user, std::string _content) {
       post_id = _post_id;
       user = _user;
       content = _content;
--- src/blogger.cpp
+++ src/blogger.cpp
@@ -1,5 +1,6 @@
 #include <blogger.hpp>
 
+// dfthomework3
 void blogger::addpost(eosio::name user, std::string content){
     deal_table table(_self, _self.value);
     deal tmp_post(table.available_primary_key(), user, content);
@@ -8,8 +9,16 @@ void blogger::addpost(eosio::name user, std::string content){
     })
 }
 void blogger::deletepost(uint64_t id, eosio::name user){
+    require_auth(_self);
+    deal_table table(_self, _self.value);
+    auto itr = table.find(id);
+    eosio::check(itr != table.end(), "A post does not exist with this ID");
+    eosio::check(has_auth(itr->get_user()) || has_auth(_self), "Missing authority to cancel this deal");
+    table.erase(itr);
 }
 void blogger::ratepost(uint64_t id, eosio::name user, int rating){
+    require_auth(_self);
+    deal_table table(_self, _self.value);
 }
 void blogger::on_transfer(eosio::name from, eosio::name to, eosio::asset quantity, std::string memo){
 }
