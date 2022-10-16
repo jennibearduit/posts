8a747dd9dbd630baff0966b4f80c5377bd143cd5
jennibearduit <77873928+jennibearduit@users.noreply.github.com>
2022-10-17 01:36:07 +0800

    implement ratepost

--- src/blogger.cpp
+++ src/blogger.cpp
@@ -19,15 +19,18 @@ void blogger::deletepost(uint64_t id, eosio::name user){
 
 void blogger::ratepost(uint64_t id, eosio::name user, int rating_score){
     require_auth(_self);
-    rating_table table(_self, _self.value);
-    rating tmp_rating(table.available_primary_key(), id, user, rating_score);
-    auto itr = table.find(id); // find if there's existing rating for the post id
-    eosio::check(itr == table.end(), "There is already an existing rating for this post");
+    rating_table table(user, user.value);
+    auto itr = table.begin();
+    while (itr != table.end()) {
+        eosio::check(itr->get_post_id() != id, "There is already an existing rating for this post by user");
+        ++ itr;
+    }
     eosio::check(rating_score >= 1 && rating_score <= 5, "Rating must be an integer between 1 to 5");
+    rating tmp_rating(table.available_primary_key(), id, user, rating_score);
     table.emplace(_self, [&](auto & entry) {
         entry = tmp_rating;
     });
-    require_recipient(user);
+    // TODO: inform post creator
 }
 
 void blogger::on_transfer(eosio::name from, eosio::name to, eosio::asset quantity, std::string memo){
